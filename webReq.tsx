e412afbce29908a203f9f1bc78fc1c0d746e7173
Vill-V <106793304+vill-v@users.noreply.github.com>
2022-06-30 19:56:59 -0700

    test connection to sign-in api

--- webReq.tsx
+++ webReq.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Button, ScrollView, Text, View } from "react-native";
+import TestData from "./testdata";
+
+const cookies = {
+  "_MHYUUID": "9999f00d-ab12-123a-12a1-a0b1c2d3e4f5",
+  "mi18nLang": "en-us",
+  "account_id": "000012345",
+  "cookie_token": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "ltoken": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "ltuid": "123456789"
+}
+
+const sim = 0;
+const indexUrl = "https://act.hoyolab.com/bbs/event/signin-bh3/index.html?act_id=e202110291205111";
+const infoUrl = "https://sg-public-api.hoyolab.com/event/mani/info?lang=en-us&act_id=e202110291205111";
+const signUrl = "https://sg-public-api.hoyolab.com/event/mani/sign?lang=en-us&act_id=e202110291205111";
 
 async function req(self:WebReq){
-  const url = "http://192.168.0.15:3000/";
+  const url = sim ? "http://192.168.0.15:3000/" : infoUrl;
   let res;
   try{
-    res = await fetch(new Request(url));
+    res = await fetch(new Request(url, {
+      method: "GET",
+      headers: new Headers(TestData().getheaders)
+    }));
     self.setState({"hello":await res.text()});
   }
   catch (e) {
@@ -22,6 +40,12 @@ export default class WebReq extends React.Component{
   };
   constructor(props) {
     super(props);
+    cookies._MHYUUID = TestData().cookie._MHYUUID;
+    cookies.account_id = TestData().cookie.account_id;
+    cookies.cookie_token = TestData().cookie.cookie_token;
+    cookies.ltoken = TestData().cookie.ltoken;
+    cookies.ltuid = TestData().cookie.ltuid;
+    cookies.mi18nLang = TestData().cookie.mi18nLang;
     this.state= {hello:"hello"};
   };
   render() {
